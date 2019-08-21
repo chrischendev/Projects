c75e0392016f077998d8fce924fbb76c10494bbb
chris <chrischen2018@163.com>
2019-08-22 01:49:23 +0800

    mod

---
+++ FileRenameTool/FileRenameTool/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FileRenameTool.models;
+
+namespace FileRenameTool
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        private void init()
+        {
+            //            Image.Source = new BitmapImage(new Uri("../res/sh_02.jpg",UriKind.RelativeOrAbsolute));
+            //            Image.Stretch = Stretch.Fill;
+
+            ObservableCollection<FileInfo> fileInfos = new ObservableCollection<FileInfo>();
+            fileInfos.Add(new FileInfo(1,"文件01.txt","",".txt"));
+            fileInfos.Add(new FileInfo(2,"文件02.txt","",".txt"));
+
+            ListView.ItemsSource = fileInfos;
+        }
+
+        private void BtnQuit_OnClick(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void BtnSelectFolder_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BtnRefreshListView_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FileRenameTool/FileRenameTool/models/FileInfo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileRenameTool.models
+{
+    class FileInfo
+    {
+        private Int64 _id;
+        private string _sourceFileName;
+        private string _targetFileName;
+        private string _fileExtName;
+
+        public FileInfo()
+        {
+        }
+
+        public FileInfo(long id, string sourceFileName, string targetFileName, string fileExtName)
+        {
+            _id = id;
+            _sourceFileName = sourceFileName;
+            _targetFileName = targetFileName;
+            _fileExtName = fileExtName;
+        }
+
+        public long Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string SourceFileName
+        {
+            get => _sourceFileName;
+            set => _sourceFileName = value;
+        }
+
+        public string TargetFileName
+        {
+            get => _targetFileName;
+            set => _targetFileName = value;
+        }
+
+        public string FileExtName
+        {
+            get => _fileExtName;
+            set => _fileExtName = value;
+        }
+    }
+}
